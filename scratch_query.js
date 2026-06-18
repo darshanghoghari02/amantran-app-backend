48fdbbf5d22e9bb6a6391cf1bd8734d4a2f7535b
darshanghoghari02 <drashanghoghari@gmail.com>
2026-06-18 16:14:04 +0530

    stage3 comp

--- scratch_query.js
+++ scratch_query.js
@@ -1,11 +1,13 @@
 import { dbService } from './src/services/db.js';
+import fs from 'fs/promises';
 import dotenv from 'dotenv';
 dotenv.config();
 
 async function run() {
   await dbService.initPromise;
-  const users = await dbService.getAll('users');
-  console.log("Current Users in DB:", JSON.stringify(users, null, 2));
+  const templates = await dbService.getAll('templates');
+  await fs.writeFile('temp_templates.json', JSON.stringify(templates, null, 2), 'utf-8');
+  console.log("Dumped " + templates.length + " templates to temp_templates.json");
   process.exit(0);
 }
 
